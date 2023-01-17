3ffed5d7eda7cde39767096339ac2f4b9b627d02
Shuydo <shuyd012@gmail.com>
2023-01-17 06:50:01 +0200

    finish server

---
+++ readme_shu.txt
@@ -0,0 +1,5 @@
+http://localhost:5000/ //Это только мой мир!
+
+https://socket.io/docs/v4/rooms/
+
+npm run dev (server start)
---
+++ utils.js
@@ -0,0 +1,10 @@
+const trimStr = str => str.trim().toLowerCase();
+
+// module.exports = trimStr;
+// export default trimStr;
+// exports.trimStr = trimStr;
+
+// const _trimStr = trimStr;
+// export { _trimStr as trimStr };
+
+exports.trimStr = trimStr;
